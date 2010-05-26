29282f21140d984027e246c12a522e1fba035902
Tuomas Kareinen <tkareine@gmail.com>
2010-05-26 17:10:24 +0300

    Symbolize database type argument

--- lib/shnotes/notes.rb
+++ lib/shnotes/notes.rb
@@ -3,7 +3,7 @@ require "shnotes/notes/pstore"
 module Shnotes
   module Notes
     def self.init_db(db_type, *args)
-      case db_type
+      case db_type.to_sym
       when :pstore
         Notes::PStore.new(*args)
       else
