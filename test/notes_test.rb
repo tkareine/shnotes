a85c9f32463665bff29799bacffec79ee212de34
Tuomas Kareinen <tkareine@gmail.com>
2010-05-26 20:19:25 +0300

    Combine PStore and Redis tests
    Their interfaces must be equal, so it is more DRY to combine the tests.

--- test/notes_test.rb
+++ test/notes_test.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 require "shnotes/notes"
+require "shnotes/notes/pstore"
+require "shnotes/notes/redis"
 
 module Shnotes
   class NotesTest < Test::Unit::TestCase
@@ -7,18 +9,67 @@ module Shnotes
       assert_raise(ArgumentError) { Shnotes::Notes.init_db(:no_such_db) }
     end
 
-    context "for PStore instance" do
-      setup do
-        @temp_file = Tempfile.new("test_notes.pstore")
-      end
+    {
+      :pstore => {
+        :type      => Shnotes::Notes::PStore,
+        :init_args => [Tempfile.new("test_notes.pstore").path]
+      },
+      :redis  => {
+        :type      => Shnotes::Notes::Redis,
+        :init_args => [{:db => 1}]
+      }
+    }.each do |db_type, spec|
+      context "for #{spec[:type]} instance" do
+        setup do
+          @notes = Shnotes::Notes.init_db(db_type, *spec[:init_args])
+        end
 
-      teardown do
-        @temp_file.close(true)
-      end
+        teardown do
+          @notes.clear
+        end
 
-      should "create instance" do
-        notes = Shnotes::Notes.init_db(:pstore, @temp_file.path)
-        assert_equal Shnotes::Notes::PStore, notes.class
+        should "create proper instance" do
+          assert_equal spec[:type], @notes.class
+        end
+
+        context "with no content" do
+          should "contain empty hash" do
+            assert_equal({}, @notes.all)
+          end
+
+          should "store a note" do
+            assert_nil @notes["1"]
+            assert_equal "foo", @notes["1"] = "foo"
+            assert_equal "foo", @notes["1"]
+            assert_equal({"1" => "foo"}, @notes.all)
+          end
+        end
+
+        context "with content" do
+          setup do
+            @notes["1"] = "bar"
+            @notes["2"] = "zag"
+          end
+
+          should "have proper contents" do
+            assert_equal({"1" => "bar", "2" => "zag"}, @notes.all)
+          end
+
+          should "delete a note" do
+            assert_equal "bar", @notes.delete("1")
+            assert_equal({"2" => "zag"}, @notes.all)
+          end
+
+          should "replace a note" do
+            assert_equal "zomg", @notes["1"] = "zomg"
+            assert_equal "zomg", @notes["1"]
+          end
+
+          should "clear all notes" do
+            assert_equal({}, @notes.clear)
+            assert_equal({}, @notes.all)
+          end
+        end
       end
     end
   end
